d8c4ec80934bf46380cba188d483d92d79e12b3b
ppx <admin@skvm.win>
2018-01-04 16:32:05 +0800

    Update one-key-kms-debian.sh

--- one-key-kms-debian.sh
+++ one-key-kms-debian.sh
@@ -30,6 +30,8 @@ echo "Addtionally, you may want to get a script to manage it."
 echo "In this case, you may download a script at the one-key-kms repository."
 echo "Thanks for your using!"
 echo "Dakkidaze <gioku@docomo.net.cn>"
+cd
+./kms.sh
 }
 echo "This script will automatically download and compile KMS Server program for you."
 echo "For more information, please visit https://github.com/Wind4/vlmcsd"
