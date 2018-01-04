d443f657cc2b6e79f384eb9c9bab6b7b9aac4603
ppx <admin@skvm.win>
2018-01-04 16:36:33 +0800

    Update one-key-kms-centos.sh

--- one-key-kms-centos.sh
+++ one-key-kms-centos.sh
@@ -31,7 +31,7 @@ echo "In this case, you may download a script at the one-key-kms repository."
 echo "Thanks for your using!"
 echo "Dakkidaze <gioku@docomo.net.cn>"
 cd
-./kms.sh
+./kms.sh start
 }
 echo "This script will automatically download and compile KMS Server program for you."
 echo "For more information, please visit https://github.com/Wind4/vlmcsd"
